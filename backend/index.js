9bb50060b5e04f7b2e0e5202741097cac134e089
Syed Masood <masoodsyed123786@gmail.com>
2025-05-10 18:46:09 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -27,5 +27,5 @@ app.use("/api/scrape", scrapeRoutes);
 app.use("/api/save", email);
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
