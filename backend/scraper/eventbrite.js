90bee9e0ba8b5fb50ee9ef502373e4ee0c380de3
Sayyed Masud <84665073+syedsm@users.noreply.github.com>
2025-05-21 22:40:30 +0530

    Update eventbrite.js

--- backend/scraper/eventbrite.js
+++ backend/scraper/eventbrite.js
@@ -3,6 +3,8 @@ const puppeteer = require('puppeteer');
 async function scrapeEventbrite() {
   const browser = await puppeteer.launch({
     headless: "new",
+        executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
+
     args: ['--no-sandbox', '--disable-setuid-sandbox'],
   });  const page = await browser.newPage();
 
